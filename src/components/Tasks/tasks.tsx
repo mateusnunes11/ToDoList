bb89997cd6a5636b34f276763dcd810aee49151b
Mateus Nunes <matnunes2018@gmail.com>
2023-03-16 17:02:06 -0300

    first commit

---
+++ src/components/Tasks/tasks.tsx
@@ -0,0 +1,54 @@
+import { TbClipboard, TbClipboardText } from "react-icons/tb";
+import { ITask } from "../../App";
+import { Task } from "../Task/task";
+import styles from "./tasks.module.css";
+
+interface Props {
+  tasks: ITask[];
+  onDelete: (taskId: string) => void;
+  onComplete: (taskId: string) => void;
+}
+
+export function Tasks({ tasks, onDelete, onComplete }: Props) {
+  const tasksQuantity = tasks.length;
+  const completedTasks = tasks.filter((task) => task.isCompleted).length;
+
+  return (
+    <section className={styles.task}>
+      <header className={styles.header}>
+        <div>
+          <p>Tarefas criadas</p>
+          <span>{tasksQuantity}</span>
+        </div>
+
+        <div>
+          <p className={styles.textPurple}>Concluídas</p>
+          <span>
+            {completedTasks} de {tasksQuantity}
+          </span>
+        </div>
+      </header>
+
+      <div className={styles.list}>
+        {tasks.map((task) => (
+          <Task
+            key={task.id}
+            task={task}
+            onDelete={onDelete}
+            onComplete={onComplete}
+          />
+        ))}
+
+        {tasks.length <= 0 && (
+          <section className={styles.empty}>
+            <TbClipboardText size={50} />
+            <div>
+              <p>Você ainda não tem tarefas cadastradas</p>
+              <p>Crie tarefas e organize seus itens a fazer</p>
+            </div>
+          </section>
+        )}
+      </div>
+    </section>
+  );
+}
